05d0783a92ab7c68f1cfaef6c6224724ad7571c9
Saurabh Aryal <saurabharyal10@gmail.com>
2024-03-10 08:04:13 +0545

    Initial Commit

---
+++ MobileProgrammingProjects/app/src/main/java/com/example/mobileprogrammingprojects/HomeFragment.java
@@ -0,0 +1,45 @@
+package com.example.mobileprogrammingprojects;
+
+import android.os.Bundle;
+
+import androidx.appcompat.widget.AppCompatSpinner;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class HomeFragment extends Fragment {
+
+    private AppCompatSpinner spnCountry;
+    private View mainView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mainView = inflater.inflate(R.layout.fragment_home, container, false);
+        findViews();
+        initToolbar();
+        return mainView;
+    }
+
+    void initToolbar() {
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_dropdown_item,
+                getResources().getStringArray(R.array.country));
+        spnCountry.setAdapter(adapter);
+    }
+    private void findViews() {
+        spnCountry = mainView.findViewById(R.id.spnCountry);
+
+    }
+
+}
