8f82a0ad04886fd124281045173a8ac063ce5e87
Saurabh Aryal <saurabharyal10@gmail.com>
2024-03-25 07:15:10 +0545

    Initial Commit

---
+++ SecondDemoApp/app/main/java/com/example/project/ViewProfileActivity.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewprofileacticity);
+    }
+}
