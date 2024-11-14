cab5c583cd9f092e5ad47dc1427072d69c9f58be
Saurabh Aryal <saurabharyal10@gmail.com>
2024-11-14 08:28:14 +0545

    Initial Phase Update

---
+++ Social Media App/app/src/main/java/com/neatroots/samplesocial/NotificationFragment.java
@@ -0,0 +1,49 @@
+package com.neatroots.samplesocial;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+
+public class NotificationFragment extends Fragment {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    public NotificationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_notification, container, false);
+
+        viewPager = view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentAdapter(getFragmentManager()));
+
+        tabLayout = view.findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return view;
+    }
+}
