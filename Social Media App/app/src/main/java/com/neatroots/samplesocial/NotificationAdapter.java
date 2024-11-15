adf8be2dbed4fe0d9368c2ffb4f7434ca148aa24
Saurabh Aryal <saurabharyal10@gmail.com>
2024-11-15 08:02:37 +0545

    Initial Phase Update

---
+++ Social Media App/app/src/main/java/com/neatroots/samplesocial/NotificationAdapter.java
@@ -0,0 +1,58 @@
+package com.neatroots.samplesocial;
+
+import android.content.Context;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.viewHolder> {
+
+    ArrayList<NotificationModel> list;
+    Context context;
+
+    public NotificationAdapter(ArrayList<NotificationModel> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.notification_rv_design, parent, false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        NotificationModel model = list.get(position);
+        holder.profile.setImageResource(model.getProfile());
+        holder.notification.setText(Html.fromHtml(model.getNotification()));
+        holder.time.setText(model.getTime());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+        ImageView profile;
+        TextView notification, time;
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            profile = itemView.findViewById(R.id.notificationProfile);
+            notification = itemView.findViewById(R.id.notification);
+            time = itemView.findViewById(R.id.time);
+
+        }
+    }
+}
