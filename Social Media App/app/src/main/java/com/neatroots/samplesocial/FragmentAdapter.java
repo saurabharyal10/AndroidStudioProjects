92fb6f6309995e21ceb2d1b2fb6f6e8dbb9114f9
Saurabh Aryal <saurabharyal10@gmail.com>
2024-11-16 21:18:27 +0545

    Initial Phase Update

---
+++ Social Media App/app/src/main/java/com/neatroots/samplesocial/FragmentAdapter.java
@@ -0,0 +1,47 @@
+package com.neatroots.samplesocial;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+
+    public FragmentAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: return new NotificationFrag();
+            case 1: return new RequestFrag();
+            default: return new NotificationFrag();
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = null;
+        if(position == 0)
+        {
+            title = "Notification";
+        }
+        else if(position == 1)
+        {
+            title = "Requests";
+        }
+        return title;
+    }
+}
