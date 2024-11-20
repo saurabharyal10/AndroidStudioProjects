50c1f1068e6b13db9cb658a4a4eaa3be2560c1ae
Saurabh Aryal <saurabharyal10@gmail.com>
2024-11-20 08:00:09 +0545

    Initial Phase Update

---
+++ Social Media App/app/src/main/java/com/neatroots/samplesocial/FriendsModel.java
@@ -0,0 +1,17 @@
+package com.neatroots.samplesocial;
+
+public class FriendsModel {
+    int Profile;
+
+    public FriendsModel(int profile) {
+        Profile = profile;
+    }
+
+    public int getProfile() {
+        return Profile;
+    }
+
+    public void setProfile(int profile) {
+        Profile = profile;
+    }
+}
